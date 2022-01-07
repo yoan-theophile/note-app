32908dbb3200f6b99c71ef2e52a588a04f870611
Theophile Tagne <tagnepierre9@gmail.com>
2022-01-07 13:59:39 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ThemeProvider } from "@mui/material/styles";
+import { routes } from "./routes";
+import { useRoutes } from "react-router";
+import theme from "./theme";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(): JSX.Element {
+  const routing = useRoutes(routes);
+  return <ThemeProvider theme={theme}>{routing}</ThemeProvider>;
 }
 
 export default App;
